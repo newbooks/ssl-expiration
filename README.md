3f100d7fc9e9cffff44377fd36455dca0f83e26f
newbooks <10424666+newbooks@users.noreply.github.com>
2025-02-26 09:39:54 -0500

    working prototype

---
+++ README.md
@@ -0,0 +1,34 @@
+# ssl-expiration
+
+This is a script to detect the expiration date of a web site.
+
+It takes in the website from command line with the option to:
+- report the only sites whose expiration date is within certain days
+
+Examples:
+1. Query a single site
+```
+ssl-expiration.py example.com
+```
+
+2. Query multiple sites
+```
+ssl-expiration.py example1.com example1.com example2.com example3.com
+```
+
+3. Query multiple sites specified in file
+sites.txt:
+```
+example1.com
+example2.com
+example3.com
+```
+Command:
+```
+ssl-expiration.py < sitest.txt
+```
+
+4. Only report sites that will expire with 14 days
+```
+ssl-expiration.py -e 14 < sitest.txt
+```
---
+++ ssl-expiration.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import ssl
+import socket
+from datetime import datetime
+
+def ssl_expiry_datetime(hostname: str) -> datetime:
+    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+
+    context = ssl.create_default_context()
+    conn = context.wrap_socket(
+        socket.socket(socket.AF_INET),
+        server_hostname=hostname,
+    )
+    # 3 second timeout because Lambda has runtime limitations
+    conn.settimeout(3.0)
+
+    conn.connect((hostname, 443))
+    ssl_info = conn.getpeercert()
+    # parse the string from the certificate into a Python datetime object
+    return datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+
+if __name__ == '__main__':
+    hostname = 'math.levich.net'
+    print(ssl_expiry_datetime(hostname))
