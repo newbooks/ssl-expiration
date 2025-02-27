c438653f77f22759196c78bd3c045bb45984121f
Junjun Mao <10424666+newbooks@users.noreply.github.com>
2025-02-27 11:11:35 -0500

    closes #7

--- ssl-expiration.py
+++ ssl-expiration.py
@@ -24,14 +24,23 @@ def ssl_expiry_datetime(hostname: str) -> datetime:
 if __name__ == '__main__':
     helpmsg = "Check SSL certificate expiration for given domains"
     parser = argparse.ArgumentParser(description=helpmsg)
-    parser.add_argument('domains', metavar='DOMAIN', type=str, nargs='+',
+    parser.add_argument('domains', metavar='domain', type=str, default=[], nargs='+',
                         help='Domain names to check')
-    parser.add_argument('--days', type=int, default=9999,
+    parser.add_argument('-d', '--days', metavar='days', type=int, default=9999,
                         help='Warn if certificate expiration is within specified days')
+    parser.add_argument('-l', '--load', metavar='file', type=str, default=None,
+                        help='Load domains from file')
     args = parser.parse_args()
 
-    for domain in args.domains:
+    # Load domains from file
+    if args.load:
+        with open(args.load) as f:
+            domains = args.domains + f.read().splitlines()
+    else:
+        domains = args.domains
+
+    for domain in domains:
         expires = ssl_expiry_datetime(domain)
         remaining = expires - datetime.now()
         if remaining < timedelta(days=args.days):
-            print(f'{domain} SSL certificate will expire in {remaining.days} days')
+            print(f'{domain:20s} expires in {remaining.days:4d} days')
